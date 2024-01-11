1c2b1586e2fb74c2a79d52b5fec5fc3a43ba3b22
rajeshrah22 <rajeshrah22@gmail.com>
2024-01-11 16:24:31 -0500

    changed config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/work/tomcat/webapps/BusAppUI', emptyOutDir: true,
+    outDir: '/work/tomcat/webapps/BusAppUI',
   },
 })
