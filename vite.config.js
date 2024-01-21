61e1255398e33faed2a7ace49321659be2d21661
rajeshrah22 <rajeshrah22@gmail.com>
2024-01-20 21:29:54 -0500

    added pre build cleanup plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { rimraf } from 'rimraf'
+
+// Custom Rollup plugin
+const preBuildCleanupPlugin = {
+  name: 'pre-build-cleanup',
+  buildStart() {
+    rimraf.sync('/work/eclipse-workspace/BusApp/src/main/webapp/assets');
+    rimraf.sync('/work/eclipse-workspace/BusApp/src/main/webapp/index.html');
+    console.log('Pre-build cleanup complete!')
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/BusApp/',
-  plugins: [react()],
+  plugins: [react(), preBuildCleanupPlugin],
   build: {
     outDir: '/work/eclipse-workspace/BusApp/src/main/webapp',
   },
